ec8ee0ff57e3f120e7dd457f4008777c3c2bad01
Greg Cotten <greg@gregcotten.com>
2025-03-05 09:33:59 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-system", from: "1.3.0"),
-        .package(url: "https://github.com/apple/swift-async-algorithms.git", exact: "1.0.1"),
+        .package(url: "https://github.com/apple/swift-async-algorithms.git", from: "1.0.1"),
         .package(url: "https://github.com/apple/swift-atomics.git", from: "1.2.0"),
         .package(url: "https://github.com/apple/swift-collections.git", from: "1.1.2"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
